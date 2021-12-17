e032312c7d1ae17753c116dfca4ee34326822a93
inbruk <35333872+inbruk@users.noreply.github.com>
2021-12-17 22:55:35 +0300

    .

--- EFNetCore6.Auxiliary/Bl/ICRUDRepository.cs
+++ EFNetCore6.Auxiliary/Bl/ICRUDRepository.cs
@@ -6,8 +6,20 @@ using System.Threading.Tasks;
 
 namespace EFNetCore6.Auxiliary.BL
 {
-    public interface ICRUDRepository<T>
+    public interface ICRUDRepository<DTO, TID>
     {
+        List<DTO> ReadAll();
 
+        void Create(DTO newDTO);
+        DTO CreateAndGet(DTO newDTO);
+        DTO Read(TID id);
+        void Update(DTO dtoItem);
+        void Delete(TID id);
+                
+        void Create(List<DTO> newDTOList);
+        List<DTO> CreateAndGet(List<DTO> newDTOList);
+        List<DTO> Read(List<TID> idList);
+        void Update(List<DTO> dtoList); 
+        void Delete(List<TID> idList);
     }
 }

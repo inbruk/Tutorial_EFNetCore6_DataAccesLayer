8bb7ef52c26242af0357185fec57180ca6b0f06a
inbruk <35333872+inbruk@users.noreply.github.com>
2022-01-16 04:10:59 +0300

    .

---
+++ EFNetCore6.DL/Migrations/20220114203633_AddFunction.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFNetCore6.DL.Migrations
+{
+    public partial class AddFunction : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                CREATE FUNCTION fnAverageAge ()
+                  RETURNS INT
+                AS
+                  BEGIN
+                    DECLARE @today DATETIME
+                    DECLARE @result INT
+                    SELECT @today = GETDATE()
+                    SELECT @result = FLOOR(AVG(FLOOR(DATEDIFF(day, BirthDate, @today) / 365.2425)))
+                      FROM Persons
+                      WHERE BirthDate IS NOT NULL
+                    RETURN @result
+                  END
+                GO
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DROP FUNCTION fnAverageAge
+                GO
+            ");
+        }
+    }
+}
